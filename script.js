835c26e3f8e5174b25a6e3ef4a3a07accf859752
Livni <llmc0383@gmail.com>
2023-07-12 21:46:08 -0600

    referencias de los botones y el formulario

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    //Se obtienen las referencias a los elementos relevantes en el documento HTML
+    const addInputButton = document.querySelector('.btn-primary'); // Botón "Agregar Input"
+    const removeInputButton = document.querySelector('.btn-danger'); // Botón "Eliminar Input"
+    const form = document.getElementById('Formulario'); // Formulario
+
+    
+    
+
+)}
